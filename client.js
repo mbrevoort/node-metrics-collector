569d81b80e7efa31860864993c6e66c030d563be
Mike Brevoort <mike@brevoort.com>
2011-10-08 18:52:57 -0600

    Added simple TCP client and baseClient shared between tcp and udp

--- client.js
+++ client.js
@@ -1,9 +1,15 @@
 var	HOST = "localhost"
-  , PORT = 40000
-  , client = require('./lib/client.js').create(PORT, HOST);
+  , udpClient = require('./index').client.udp.create(40000, HOST, true)
+  , tcpClient = require('./index').client.tcp.create(10000, HOST, true);
 
-client.incCounter("foo.joe.counter", 1);
-client.markMeter("foo.joe.meter", Math.ceil(Math.random() * 100));
-client.updateDecayHistogram("foo.joe.decayhist", Math.ceil(Math.random() * 100));
-client.updateHistogram("foo.joe.hist", Math.ceil(Math.random() * 100));
-client.updateTimer("foo.joe.timer", Math.ceil(Math.random() * 100));
+udpClient.incCounter("foo.joe.counter", 1);
+udpClient.markMeter("foo.joe.meter", Math.ceil(Math.random() * 100));
+udpClient.updateDecayHistogram("foo.joe.decayhist", Math.ceil(Math.random() * 100));
+udpClient.updateHistogram("foo.joe.hist", Math.ceil(Math.random() * 100));
+udpClient.updateTimer("foo.joe.timer", Math.ceil(Math.random() * 100));
+
+tcpClient.incCounter("bar.joe.counter", 1);
+tcpClient.markMeter("bar.joe.meter", Math.ceil(Math.random() * 100));
+tcpClient.updateDecayHistogram("bar.joe.decayhist", Math.ceil(Math.random() * 100));
+tcpClient.updateHistogram("bar.joe.hist", Math.ceil(Math.random() * 100));
+tcpClient.updateTimer("bar.joe.timer", Math.ceil(Math.random() * 100));
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports.client = require('./lib/client');
 module.exports.state = require('./lib/state');
+
 module.exports.collector = {};
 module.exports.collector.udp = require('./lib/collector/udp');
 module.exports.collector.tcp = require('./lib/collector/tcp');
@@ -9,3 +9,7 @@ module.exports.harvester = require('./lib/harvester');
 module.exports.publisher = {};
 module.exports.publisher.disk = require('./lib/publisher/disk');
 module.exports.publisher.graphite = require('./lib/publisher/graphite');
+
+module.exports.client = {};
+module.exports.client.udp = require('./lib/client/udp');
+module.exports.client.tcp = require('./lib/client/tcp');
